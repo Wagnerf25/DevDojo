58539f2db8020a3e2dd42de270ea6e2eb84bb144
Wagner <wagner.rf26@gmail.com>
2025-09-21 17:52:23 -0300

    09 - Comentários

---
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * isso é um comentário <b>javadoc</b>
+ */
+public class OlaDevDojo {
+    // isso é um comentário de uma linha
+    /*
+        isso é um comentário
+        de multiplas
+        linhas
+     */
+
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
